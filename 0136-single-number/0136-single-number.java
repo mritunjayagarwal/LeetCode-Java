7d40f46d8506e4aad5da35371987f209969053fc
Mritunjay Agarwal <39786312+mritunjayagarwal@users.noreply.github.com>
2022-11-14 20:54:48 +0530

    Time: 2 ms (85.94%), Space: 51.1 MB (28.92%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,16 +3,26 @@ class Solution {
         if(nums.length == 1){
             return nums[0];
         }
-
-        Set<Integer> set = new HashSet<>();
-        for(int i = 0; i <= nums.length - 1; i++){
-            if(set.contains(nums[i])){
-                set.remove(nums[i]);
-            }else{
-                set.add(nums[i]);
-            }
+        
+        
+// Using XOR gate for Duplicate Detection
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ans = ans ^ nums[i];
         }
+        
+        return ans;
+        
+// Bruteforce Method
+//         Set<Integer> set = new HashSet<>();
+//         for(int i = 0; i <= nums.length - 1; i++){
+//             if(set.contains(nums[i])){
+//                 set.remove(nums[i]);
+//             }else{
+//                 set.add(nums[i]);
+//             }
+//         }
 
-        return set.iterator().next();
+//         return set.iterator().next();
     }
 }
